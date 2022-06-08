35b6f4b516a9563ed158d5c6b51c40bb2d7df3d1
Mwenje <mwenje24@gmail.com>
2022-06-08 09:40:30 +0300

    commit tree c7bacdf1ffca9b96edb854ef47da870e38c87f9e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driver) {
+    return driver.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function (driver) {
+    return driver.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = (multiplyValue) => {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = (drivers, driversReturn) => {
+    return driversReturn(drivers);
+  };
